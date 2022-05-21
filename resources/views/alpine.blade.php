d25380a33cb8267ca3ecc5c2d19270f1fdfe1fb1
GiovanniOliveiraDS <giovanni.oliveira@devsquad.com>
2022-05-21 02:30:40 -0300

    Add Toasty!

--- resources/views/alpine.blade.php
+++ resources/views/alpine.blade.php
@@ -9,7 +9,7 @@
         <section class="my-10">
             <h2 class="text-lg text-gray-700 dark:text-gray-200 mb-3">Toast</h2>
 
-            <div class="space-x-2">
+            <div class="space-x-2" x-data="{ anm: false }">
                 <button class="bg-green-300 p-3 rounded-lg text-green-900 transform transition duration-500 hover:scale-110"
                     x-on:click="$dispatch('notice',
                     {
@@ -50,8 +50,24 @@
                 >
                     Error
                 </button>
+
+                <button class="bg-cyan-300 p-3 rounded-lg text-cyan-900 transform transition duration-500 hover:scale-110"
+                    x-on:click="let audio = new Audio('{{ asset('toasty.mp3') }}'); audio.play(); anm = true; setTimeout(() => anm = false, 800)">
+                    Toasty!
+                </button>
+
+                <img
+                    x-show="anm"
+                    x-transition:enter="transition ease-out duration-100 origin-right"
+                    x-transition:enter-start="translate-x-6"
+                    x-transition:enter-end="translate-x-0"
+                    x-transition:leave="transition ease-in duration-100 origin-right"
+                    x-transition:leave-start="translate-x-0"
+                    x-transition:leave-end="translate-x-6"
+                    src="{{ asset('toasty.png') }}" alt=""
+                    class="fixed w-20 bottom-28 right-0 rounded"
+                >
             </div>
         </section>
-
     </div>
 @endsection
