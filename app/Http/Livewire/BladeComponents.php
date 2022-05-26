c71de56218154fcc00bf6e1e100804d5d6adcfbd
PH7-Jack <pedrolivertwd@gmail.com>
2022-05-26 18:47:59 -0300

    add blade components page

---
+++ app/Http/Livewire/BladeComponents.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BladeComponents extends Component
+{
+    public function mount()
+    {
+        $this->addError('name', 'Name is required.');
+        $this->addError('name2', 'Name is required.');
+        $this->addError('name3', 'Name is required.');
+    }
+
+    public function render()
+    {
+        return view('livewire.blade-components');
+    }
+}
