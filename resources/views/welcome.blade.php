54df8469e2881518bba13714aef8021f6fb5fb53
PH7-Jack <pedrolivertwd@gmail.com>
2022-05-26 20:21:45 -0300

    add devsquad logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout title="Welcome to the Workshop">
-    <div class="flex flex-col p-10">
-        <h1 class="text-2xl font-semibold text-gray-700 dark:text-gray-200">
+    <div class="h-full flex items-center justify-center relative">
+        <h1 class="absolute top-5 left-5 text-2xl font-semibold text-gray-700 dark:text-gray-200">
             Welcome
         </h1>
+
+        <img src="{{ asset('devsquad.svg') }}" class="opacity-60 h-[300px]">
     </div>
 </x-app-layout>
